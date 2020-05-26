1b8777145493be883ce536052aeda61d561370cb
Marc <39213657+marcplustwo@users.noreply.github.com>
2020-05-26 16:55:51 +0200

    structure project

---
+++ functions/slr.py
@@ -0,0 +1,9 @@
+def do_search(query):
+    from wrapper.springerWrap import SpringerWrapper
+
+    springer = SpringerWrapper(apiKey="***REMOVED***")
+
+    res = springer.callAPI(f'keyword: "{query}"')
+    print(res)
+
+    return res
