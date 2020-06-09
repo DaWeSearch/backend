c769d538f076adeb55770409dea9e80843357fd5
Marc <39213657+marcplustwo@users.noreply.github.com>
2020-06-09 12:21:03 +0200

    add pagination for each wrapper

--- functions/slr.py
+++ functions/slr.py
@@ -1,16 +1,35 @@
 import json
 
 
-def do_search(search):
+def do_search(search, dry=False):
+    # TODO: get all wrappers
     from wrapper.springerWrapper import SpringerWrapper
     from wrapper.elsevierWrapper import ElsevierWrapper
 
-    # TODO: get API key from user account
+    # TODO: get API key from user account. For all dbs where a key is present, search.
     springer = SpringerWrapper(apiKey="51502308cc700373e75693f79b5697aa")
     elsevier = ElsevierWrapper(apiKey="736a86104b9e47a08f0d886d4e725561")
 
-    db_wrappers = [elsevier]
+    db_wrappers = [springer]
     # db_wrappers = [springer, elsevier]
+
+    # get info about results
+    # query all databases once and get a picture of how many hits there are.
+    db_info = dict()
+    db_info = {
+        "total": int,
+        "databases": [
+            "springer": {
+
+            },
+            "elsevier": {
+
+            }
+        ]
+    }
+    for db_wrapper in db_wrappers:
+        first = db_wrapper.callAPI(search)
+
 
     records = []
 
@@ -21,15 +40,28 @@ def do_search(search):
 
         records += first['records']
 
-        start_points = [i for i in range(51, total_results, 50)]
+        page_length = int(first['result']['pageLength'])
 
+        start_points = [i for i in range(
+            page_length + 1, total_results, page_length)]
+
+        urls = []
         # only get 2 pages for now. Remove [:2] to get all
         for start in start_points[:2]:
             db_wrapper.startAt(start)
-            results = db_wrapper.callAPI(search)
-            records += results['records']
+            results = db_wrapper.callAPI(search, dry=dry)
+            if dry:
+                urls.append(results)
+            else:
+                records += results['records']
 
     return records
+
+def call_api(db_wrapper, search, page: int, page_length: int):
+    # page 1 starts at 1, page 2 at page_length + 1
+    db_wrapper.startAt((page - 1) * page_length + 1)
+    # db_wrapper.set_max_records(page_length)
+    return db_wrapper.callAPI(search)
 
 
 def conduct_query(review, search: dict):
@@ -39,7 +71,7 @@ def conduct_query(review, search: dict):
 
     # search in databases
     results = []
-    results += do_search(search)
+    results += do_search(search, dry=False)
     # results.append(search_elsevier(search))
 
     save_results(results, review, query)
@@ -50,33 +82,20 @@ def conduct_query(review, search: dict):
 
 
 if __name__ == '__main__':
-    from db.connector import add_review, new_query, save_results, get_results_for_query
-    search_terms = {
-        "search_groups": [
-            {
-                "search_terms": ["blockchain", "distributed ledger"],
-                "match": "OR"
-            }
-        ],
-        "match": "AND"
-    }
+    from db.connector import add_review, new_query, save_results, get_results_for_review, get_review_by_id
+    # search_terms = {
+    #     "search_groups": [
+    #         {
+    #             "search_terms": ["blockchain", "distributed ledger"],
+    #             "match": "OR"
+    #         }
+    #     ],
+    #     "match": "AND"
+    # }
 
-    review = add_review("test REVIEW")
-    # query = new_query(review)
-    # with open('test_results.json', 'r') as file:
-    #     results = json.load(file)
+    # review = add_review("test REVIEW")
 
-    query = conduct_query(review, search_terms)
+    # query = conduct_query(review, search_terms)
 
-    # save_results(results, review, query)
-
-    ret = get_results_for_query(query)
-    print(ret)
-    pass
-
-
-# for testing
-def populate():
-    from db.connector import add_review
-    review = add_review("test_query")
-    manage_query(review)
+    # review = get_review_by_id("5ede4708cfb4b32044d32734")
+    # res = get_results_for_review(review, page=1, page_length=50)
