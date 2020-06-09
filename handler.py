54a2a9d576bde6539f2b4480c0086d6959caa169
Marc <39213657+marcplustwo@users.noreply.github.com>
2020-06-09 19:47:57 +0200

    demo deployment

--- handler.py
+++ handler.py
@@ -4,46 +4,19 @@ import os
 # https://docs.aws.amazon.com/lambda/latest/dg/python-handler.html
 
 
-def get_reviews(event, context):
-    from functions import connector
-    connector.add_review()
-    reviews = connector.get_reviews()
-
-    # create response
-    response = {
-        "statusCode": 200,
-        "headers": {},
-        "body": json.dumps(reviews)
-    }
-
-    return response
-
-
-def review(event, context):
-    from functions.db.connector import add_review, to_dict
+def dry_query(event, context):
+    from functions.slr import dry_query
 
     body = json.loads(event["body"])
-    review_name = body["name"]
-    search = body["search"]
-    res = add_review(review_name, search)
+    search = body.get('search')
+    page = body.get('page')
+    page_length = body.get('page_length')
+
+    results = dry_query(search, page, page_length)
 
     response = {
         "statusCode": 201,
         "headers": {},
-        "body": json.dumps(res)
-    }
-    return response
-
-
-def search(event, context):
-    from functions.slr import do_search
-
-    body = json.loads(event["body"])
-    res = do_search(body["query"])
-
-    response = {
-        "statusCode": 201,
-        "headers": {},
-        "body": json.dumps(res)
+        "body": json.dumps(results)
     }
     return response
