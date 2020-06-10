e2e67229a615ffcaa32ac09d08848c782b9a3d4b
druckdev <63563978+druckdev@users.noreply.github.com>
2020-06-10 03:30:51 +0200

    Replaced comments with docstring
    See https://github.com/DaWeSys/backend/issues/10

--- wrapper/templateWrapper.py
+++ wrapper/templateWrapper.py
@@ -7,64 +7,68 @@ class TemplateWrapper(WrapperInterface):
 	def __init__(self, apiKey: str):
 		pass
 
-	# Endpoint used for the query
 	@property
 	def endpoint(self) -> str:
+		"""Return the endpoint used for the query."""
 		pass
 
-	# A dictionary that contains the available result formats for each collection
 	@property
 	def allowedResultFormats(self) -> {str: [str]}:
+		"""Return a dictionary that contains the available result formats for each collection."""
 		pass
 
-	# The result format that will be used for the query
 	@property
 	def resultFormat(self) -> str:
+		"""Return the result format that will be used for the query."""
 		pass
 
-	# resultFormat must be settable
 	@resultFormat.setter
 	def resultFormat(self, value: str):
+		"""Set the result format."""
 		pass
 
-	# Collection being used for the query
 	@property
 	def collection(self) -> str:
+		"""Return the collection in which the query searches."""
 		pass
 
-	# collection must be settable
 	@collection.setter
 	def collection(self, value: str):
+		"""Set the collection used."""
 		pass
 
-	# Maximum number of results that the API will return
 	@property
 	def maxRecords(self) -> int:
+		"""Return the maximum number of results that the API can return."""
 		pass
 
-	# Dictionary of allowed keys and their allowed values for searchField()
 	@property
 	def allowedSearchFields(self) -> {str: [str]}:
+		"""Return all allowed search parameter, value combination.
+
+		An empty array means no restrictions for the value of that key.
+		"""
 		pass
 
-	# Specify value for a given search parameter for manual search
 	def searchField(self, key: str, value):
+		"""Set the value for a given search parameter in a manual search."""
 		pass
 
-	# Reset a search parameter
 	def resetField(self, key: str):
+		"""Reset a search parameter."""
 		pass
 
-	# Translate a search in the wrapper input format into a query that the wrapper api understands
 	def translateQuery(self, query: dict) -> str:
+		"""Translate a query in the defined inputFormat into a query that the API understands."""
 		pass
 
-	# Set the index from which the returned results start
-	# (Necessary if there are more hits for a query than the maximum number of returned results.)
 	def startAt(self, value: int):
+		"""Set the index from which the returned results start."""
 		pass
 
-	# Make the call to the API
-	# If no query is given, use the manual search specified by searchField() calls
 	def callAPI(self, query: Union[dict, None], raw: bool, dry: bool):
+		"""Make the call to the API.
+
+		If no query is given build the manual search specified by searchField() calls.
+		"""
 		pass
