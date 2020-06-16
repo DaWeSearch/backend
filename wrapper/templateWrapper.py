2baf5b3d30f4ec9aecca6af3a9b6c06a08380f8a
druckdev <63563978+druckdev@users.noreply.github.com>
2020-06-17 00:25:50 +0200

    Rework docstrings
    Fix https://github.com/DaWeSys/backend/issues/10
        (Added class and module docstrings and Args and Returns sections for
        the function docstrings.)
    Added showNum and maxRetries to the wrapper interface
    corrected type hint
    README: - added hints to the all_wrappers array and to leave PRs
            - updated template sed script

--- wrapper/templateWrapper.py
+++ wrapper/templateWrapper.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
+"""A wrapper for the <DATABASE> API."""
+
+from typing import Optional
 
 from .wrapperInterface import WrapperInterface
-from typing import Union
 
 class TemplateWrapper(WrapperInterface):
+	"""A wrapper class for the <DATABASE> API."""
+
 	def __init__(self, apiKey: str):
+		"""Initialize a wrapper object.
+
+		Args:
+			apiKey: The API key that should be used for a request.
+		"""
 		pass
 
 	@property
@@ -24,7 +33,11 @@ class TemplateWrapper(WrapperInterface):
 
 	@resultFormat.setter
 	def resultFormat(self, value: str):
-		"""Set the result format."""
+		"""Set the result format.
+
+		Args:
+			value: The result format that will be set. Has to be allowed for set collection.
+		"""
 		pass
 
 	@property
@@ -34,12 +47,30 @@ class TemplateWrapper(WrapperInterface):
 
 	@collection.setter
 	def collection(self, value: str):
-		"""Set the collection used."""
+		"""Set the collection used.
+
+		Args:
+			value: The collection that will be set. Has to be an allowed value.
+		"""
 		pass
 
 	@property
 	def maxRecords(self) -> int:
 		"""Return the maximum number of results that the API can return."""
+		pass
+
+	@property
+	def showNum(self) -> int:
+		"""Return the number of results that the API will return."""
+		pass
+
+	@showNum.setter
+	def showNum(self, value: int):
+		"""Set the number of results that will be returned.
+
+		Args:
+			value: The number of results.
+		"""
 		pass
 
 	@property
@@ -50,25 +81,66 @@ class TemplateWrapper(WrapperInterface):
 		"""
 		pass
 
+	@property
+	def maxRetries(self) -> int:
+		"""Return the maximum number of retries the wrapper will do on a timeout."""
+		pass
+
+	@maxRetries.setter
+	def maxRetries(self, value: int):
+		"""Set maximum number of retries on a timeout.
+
+		Args:
+			value: Number of retries that will be set.
+		"""
+		pass
+
 	def searchField(self, key: str, value):
-		"""Set the value for a given search parameter in a manual search."""
+		"""Set the value for a given search parameter in a manual search.
+
+		Args:
+			key: The search parameter.
+			value: The value that the search parameter should have.
+		"""
 		pass
 
 	def resetField(self, key: str):
-		"""Reset a search parameter."""
+		"""Reset a search parameter.
+
+		Args:
+			key: The search parameter that shall be resetted.
+		"""
 		pass
 
 	def translateQuery(self, query: dict) -> str:
-		"""Translate a query in the defined inputFormat into a query that the API understands."""
+		"""Translate a dictionary into a query that the API understands.
+
+		Args:
+			query: A query dictionary as defined in wrapper/inputFormat.py.
+		"""
 		pass
 
 	def startAt(self, value: int):
-		"""Set the index from which the returned results start."""
+		"""Set the index from which the returned results start.
+
+		Args:
+			value: The start index.
+		"""
 		pass
 
-	def callAPI(self, query: Union[dict, None], raw: bool, dry: bool):
+	def callAPI(self, query: Optional[dict] = None, raw: bool = False, dry: bool = False):
 		"""Make the call to the API.
 
 		If no query is given build the manual search specified by searchField() calls.
+
+		Args:
+			query: A dictionary as defined in wrapper/inputFormat.py.
+				If not specified, the parameters dict modified by searchField is used.
+			raw: Should the raw request.Response of the query be returned?
+			dry: Should only the data for the API request be returned and nothing executed?
+
+		Returns:
+			If dry is True only the request data is returned.
+			If raw is False the formatted response is returned else the raw request.Response.
 		"""
 		pass
