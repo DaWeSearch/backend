18ed9d365f9326fc72f2c9a71046132af8ef97d0
Philippe Kalinowski <philippe.isb@gmail.com>
2020-06-22 14:26:39 +0200

    added loginhandler

--- functions/authentication.py
+++ functions/authentication.py
@@ -1,24 +1,40 @@
+import os
 import jwt
 
-encoded_jwt = jwt.encode({'username':'philippe'}, 'secret')
+from functions.db.models import *
+
+# TODO move to handler.py
+
+# Fetch jwt secret key env var
+# jwt_key_env = os.getenv('JWT_SECRET_KEY')
+
+# TODO comment out for local testing
+jwt_key_env = "secretKey"
+
+encoded_jwt = jwt.encode({'username':'philippe'}, jwt_key_env)
 print(encoded_jwt)
 
 
-decoded_jwt = jwt.decode(encoded_jwt, 'secret')
+decoded_jwt = jwt.decode(encoded_jwt, jwt_key_env)
 print(decoded_jwt)
 
 
 # TODO Implement PyJWT-Authorization - vielleicht auch über eine def
 
 def authenticate_user(token):
-    return jwt.decode(token, 'secret')
+    return jwt.decode(token, jwt_key_env)
 
 
 def check_for_token(token):
     try:
-        data = jwt.decode(token, 'secret')
+        data = jwt.decode(token, jwt_key_env)
     except:
         return ""
 
+
 def login(username, password):
-    token = jwt.encode({'user': username, 'exp': 'test'}, 'secret')
+    token = jwt.encode({'user': username, 'exp': 'test'}, jwt_key_env)
+
+
+def get_jwt_for_user(user: User):
+    return jwt.encode({'username': user.username}, jwt_key_env)
