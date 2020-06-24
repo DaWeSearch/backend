5a5292fb2843ef2542a67a92be3c6957778e5aa9
Marc <39213657+marcplustwo@users.noreply.github.com>
2020-06-24 16:56:34 +0200

    add new query handler

--- handler.py
+++ handler.py
@@ -1,7 +1,10 @@
 import json
 import os
 
-from functions.slr import conduct_query
+from bson import json_util
+
+from functions import slr
+from functions.db import connector
 
 # https://docs.aws.amazon.com/lambda/latest/dg/python-handler.html
 
@@ -13,7 +16,7 @@ def dry_query(event, context):
     page = body.get('page')
     page_length = body.get('page_length')
 
-    results = conduct_query(search, page, page_length)
+    results = slr.conduct_query(search, page, page_length)
 
     response = {
         "statusCode": 201,
@@ -23,5 +26,30 @@ def dry_query(event, context):
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
         "body": json.dumps(results)
+    }
+    return response
+
+
+def new_query(event, context):
+    body = json.loads(event["body"])
+
+    review_id = body.get('review_id')
+    search_terms = body.get('search_terms')
+
+    review = connector.get_review_by_id(review_id)
+    new_query = connector.new_query(review, search_terms)
+
+    resp_body = {
+        "review": review.to_son().to_dict()
+    }
+
+    response = {
+        "statusCode": 201,
+        "headers": {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        "body": json.dumps(resp_body, default=json_util.default)
     }
     return response
