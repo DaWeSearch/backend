80f57df6990d5f4b5a5fd78d26f39825fb611cc8
Marc <39213657+marcplustwo@users.noreply.github.com>
2020-06-26 13:53:40 +0200

    convert parameters to int

--- handler.py
+++ handler.py
@@ -38,18 +38,24 @@ def dry_query(event, context):
             <wrapper/outputFormat.json>
         }
     """
+    # try:
+    body = json.loads(event["body"])
+    search = body.get('search')
     try:
-        body = json.loads(event["body"])
-        search = body.get('search')
-
         page = event.get('queryStringParameters').get('page', 1)
+    except AttributeError:
+        page  = 1
+    
+    try:
         page_length = event.get('queryStringParameters').get('page_length', 50)
+    except AttributeError:
+        page_length = 50
 
-        results = slr.conduct_query(search, page, page_length)
+    results = slr.conduct_query(search, page, page_length)
 
-        return make_response(status_code=201, body=results)
-    except Exception as e:
-        return make_response(status_code=500, body={"error": e})
+    return make_response(status_code=201, body=results)
+    # except Exception as e:
+    #     return make_response(status_code=500, body={"error": e})
 
 
 def new_query(event, context):
@@ -66,24 +72,24 @@ def new_query(event, context):
             "new_query_id": new_query_id
         }
     """
-    try:
-        body = json.loads(event["body"])
+    # try:
+    body = json.loads(event["body"])
 
-        review_id = event.get('pathParameters').get('review_id')
-        review = connector.get_review_by_id(review_id)
+    review_id = event.get('pathParameters').get('review_id')
+    review = connector.get_review_by_id(review_id)
 
-        search = body.get('search')
+    search = body.get('search')
 
-        new_query = connector.new_query(review, search)
+    new_query = connector.new_query(review, search)
 
-        resp_body = {
-            "review": review.to_son().to_dict(),
-            "new_query_id": new_query._id
-        }
+    resp_body = {
+        "review": review.to_son().to_dict(),
+        "new_query_id": new_query._id
+    }
 
-        return make_response(status_code=201, body=resp_body)
-    except Exception as e:
-        return make_response(status_code=500, body={"error": str(e)})
+    return make_response(status_code=201, body=resp_body)
+    # except Exception as e:
+    #     return make_response(status_code=500, body={"error": str(e)})
 
 
 def get_persisted_results(event, context):
@@ -179,26 +185,26 @@ def persist_list_of_results(event, body):
             "success": True
         }
     """
-    try:
-        body = json.loads(event["body"])
+    # try:
+    body = json.loads(event["body"])
 
-        results = body.get('results')
+    results = body.get('results')
 
-        review_id = event.get('pathParameters').get('review_id')
-        review = connector.get_review_by_id(review_id)
+    review_id = event.get('pathParameters').get('review_id')
+    review = connector.get_review_by_id(review_id)
 
-        query_id = event.get('queryStringParameters').get('query_id')
-        query = connector.get_query_by_id(review, query_id)
+    query_id = event.get('queryStringParameters').get('query_id')
+    query = connector.get_query_by_id(review, query_id)
 
-        connector.save_results(results, review, query)
+    connector.save_results(results, review, query)
 
-        resp_body = {
-            "query_id": query_id,
-            "success": True
-        }
-        return make_response(status_code=201, body=resp_body)
-    except Exception as e:
-        return make_response(status_code=500, body={"error": str(e)})
+    resp_body = {
+        "query_id": query_id,
+        "success": True
+    }
+    return make_response(status_code=201, body=resp_body)
+    # except Exception as e:
+    #     return make_response(status_code=500, body={"error": str(e)})
 
 
 def make_response(status_code: int, body: dict):
